f67fc1567f6f5abd846c4dec97b717e78ec2a8e3
JanviLakani <lakanijanvi1999@gmail.com>
2025-06-06 09:53:04 +0530

    add06_06_class

--- js/17_full_form_velidation.js
+++ js/17_full_form_velidation.js
@@ -3,55 +3,102 @@ function validateForm(){
 
     event.preventDefault();
 
-    const name =document.contactForm.name.value;
-    const email=document.contactForm.email.value;
-    const mobile=document.contactForm.mobile.value
+    // const name =document.contactForm.name.value;
+    // const email=document.contactForm.email.value;
+    // const mobile=document.contactForm.mobile.value;
+    const country=document.contactForm.country.value;
+    const hobbies=document.contactForm.hobbies;
 
-    console.log(name);
-    console.log(email);
-    console.log(mobile);
+    // console.log(name);
+    // console.log(email);
+    // console.log(mobile);
+    // console.log(hobbies[0].checked);
+
+    console.log(country);
+    console.log(hobbies);
+    
+    
     
     
 
-    if (name == "") {
-        document.getElementById("nameErr").innerHTML="enter your name"
-    } else {
-        const regex=/^[a-zA-Z ]{2,30}$/;
+    // if (name == "") {
+    //     document.getElementById("nameErr").innerHTML="enter your name"
+    // } else {
+    //     const regex=/^[a-zA-Z ]{2,30}$/;
 
-        if (regex.test(name)) {
-            document.getElementById("nameErr").innerHTML=""
-        } else {
-             document.getElementById("nameErr").innerHTML="please enter velid mobile number"
-        }
+    //     if (regex.test(name)) {
+    //         document.getElementById("nameErr").innerHTML=""
+    //     } else {
+    //          document.getElementById("nameErr").innerHTML="please enter velid mobile number"
+    //     }
+    // }
+
+    // if(email == "") {
+    //     document.getElementById("emailErr").innerHTML="enter your email id"
+    // } else {
+    //     const regex1 =/^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+    //     if(regex1.test(email)) {
+    //          document.getElementById("emailErr").innerHTML="";
+    //     } else {
+    //         document.getElementById("emailErr").innerHTML="please enter                                                                                                                                  velid email"
+    //     }
+    // }
+
+    // ________________________________________________________ 
+
+    // use regular expressions .
+
+    // __________________________________________________________ 
+
+    // if (mobile == "") {
+    //     document.getElementById("mobileErr").innerHTML="enter your mobil number"
+    // } else {
+    //     const regex2 =/^[7-9][0-9]{9}$/;
+
+    //     if(regex2.test(mobile)) {
+    //         document.getElementById("mobileErr").innerHTML="";
+    //     } else {
+    //         document.getElementById("mobileErr").innerHTML="please enter velid email"
+    //     }
+    // }
+
+    // _________________________________________________________ 
+
+    // not use regular expression ____ email validation
+
+
+    // if (mobile == "") {
+    //     document.getElementById("mobileErr").innerHTML="enter your mobile number"
+    // } else{
+    //     if((isNaN(mobile)) || (mobile.length != 10 )) {
+    //         document.getElementById("mobileErr").innerHTML="enter your velid mobile number"
+    //     } else {
+    //         document.getElementById("mobileErr").innerHTML="";
+    //     }
+    // }
+
+    let hobbiescounter=0;
+
+    for (let i=0; i<hobbies.length; i++) {
+
+        if (hobbies[i].checked) {
+            hobbiescounter++;
+        } 
+
     }
 
-    if(email == "") {
-        document.getElementById("emailErr").innerHTML="enter your email id"
+    if (hobbiescounter < 2) {
+        document.getElementById("hobbyErr").innerHTML="please enter minimum 2 hobby";
     } else {
-        const regex1 =/^[^\s@]+@[^\s@]+\.[^\s@]+$/
-
-        if(regex1.test(email)) {
-             document.getElementById("emailErr").innerHTML="";
-        } else {
-            document.getElementById("emailErr").innerHTML="please enter                                                                                                                                  velid email"
-        }
-    }
-
-    if (mobile == "") {
-        document.getElementById("mobileErr").innerHTML="enter your mobil number"
-    } else {
-        const regex2 =/^[7-9][0-9]{9}$/;
-
-        if(regex2.test(mobile)) {
-            document.getElementById("mobileErr").innerHTML="";
-        } else {
-            document.getElementById("mobileErr").innerHTML="please enter velid email"
-        }
+        document.getElementById("hobbyErr").innerHTML="";
     }
 
 
 
 
+
+    
     
 
 }
---
+++ js/19_data_type_pre_notpre.js
@@ -0,0 +1,61 @@
+
+// data type 
+
+// (1):- primitive data type 
+
+// 1 number
+
+let x = 10;
+
+console.log(x , typeof x);
+
+// string
+
+let y = "100";
+
+console.log(y, typeof y);
+
+// 3 booleab
+
+let a = true;
+let b = false;
+
+console.log(a, typeof a);
+console.log(b, typeof b);
+
+
+// 4 bigint
+
+// je number mota ma moto hoi and last ma n add karva ma aave to e biging kevai chhe
+
+let c = 999999999999999n
+console.log(c ,typeof c);
+
+let d = 999999999999999999;
+console.log(d , typeof d);   // aama je ans aave chhe e value ma je number 
+                            //   chhe e nuthi ans alg chhe and value ...pn aavel chhe
+                            
+                            
+
+// 5 undefined 
+
+let e;
+
+console.log(e, typeof e);
+
+
+// 6 null :- koi stu raja leva no chhe and ene jan kari chhe 
+// to e null aave chhe but e aaavito to nathi jo valu nathi null chhe
+
+
+let f = null;
+
+console.log(f , typeof f);  // ans object
+
+// ans ma type of ma object aave chhe km ke null kai 
+// nathi maltu to e last ma object j bne chhe js ma 
+// koi bhi last ma object j hoi chhe 
+
+
+
+
