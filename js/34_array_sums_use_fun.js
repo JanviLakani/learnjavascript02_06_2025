7ccfe05e1b02c8b0f3b7184a83ce82adb2e44eab
JanviLakani <lakanijanvi1999@gmail.com>
2025-06-18 23:21:17 +0530

    add_home_16_06_array_task

--- js/34_array_sums_use_fun.js
+++ js/34_array_sums_use_fun.js
@@ -1,3 +1,4 @@
+// ___________________________________________________________________ 1
 
 // 1.    Finding the maximum element in an array. 
 
@@ -20,6 +21,7 @@
 // console.log(arr);
 
 
+// ___________________________________________________________________ 2
 
 
 // 2.    Finding the minimum element in an array.
@@ -31,6 +33,7 @@
 
 
 // class 
+// withoufunction 
 
 // const arr=Math.min(10,50,99,87,58,42,38,98);
 //  max=arr[0]
@@ -41,6 +44,7 @@
 //  }
 
 // console.log(arr);
+// ___________________________________________________________________ 3
   
 
 // 3.    Sorting an array in ascending order. 
@@ -52,6 +56,7 @@
 // console.log(ans);  // ans [10, 38, 42, 50, 58, 87, 98, 99] ascending
 
 // class 
+// withoufunction 
 
 // const arr=[10,50,99,87,58,42,38,98]; 
 //         // i  j 
@@ -71,6 +76,7 @@
 
 
 
+// ___________________________________________________________________ 4
 
 
 
@@ -83,6 +89,16 @@
 
 // console.log(ans);  // ans [99, 98, 87, 58, 50, 42, 38, 10] descending
 
+// withoufunction 
+
+
+
+
+
+
+
+// ___________________________________________________________________ 5
+
 
 // 5.    Reversing an array. 
 
@@ -94,6 +110,7 @@
 
 
 // class 
+// withoufunction 
 
 // const arr=[10,20,30,40,50,60,70,80,90,100];
 // let arr1=[];
@@ -108,6 +125,7 @@
 
 
 
+// ___________________________________________________________________ 6
 
 
 // 6.    Finding the sum of all elements in an array. 
@@ -118,6 +136,8 @@
 
 // console.log(ans);  // ans 550  sum of all elements
 
+// withoufunction 
+
 // const arr=[10,20,30,40,50,60,70,80,90,100];
 
 // let sum=0;
@@ -127,6 +147,7 @@
 
 // console.log(sum );
 
+// ___________________________________________________________________ 7
 
 
 // 7.    Finding the average of all elements in an array. 
@@ -177,6 +198,7 @@
 
 // console.log(average);    // ans 55 average of all elements
 
+// ___________________________________________________________________ 8
 
 
 // 8.    Checking if an array contains a specific element.  // means ki find
@@ -239,6 +261,7 @@
 
 // console.log(ans)    // ans [20]
 
+// ___________________________________________________________________ 9
 
 // 9.    Removing duplicates from an array. 
 
@@ -254,6 +277,7 @@
 
 //  console.log(arr,unicnum);   // ans [10, 50, 99, 87, 58, 42, 38, 98]
 
+// ___________________________________________________________________ 10
 
 //  10.    Merging two arrays into a new array. 
 
@@ -264,19 +288,278 @@
 
 // console.log(ans);  // ans [10, 20, 30, 40, 50, 11, 22, 33, 44, 55]
 
-
-const arr=[10,20,30,40,50];
-const arr1=[11,22,33,44,55];
-
-let ans=[];
-
-for(let i=0; i<=arr.length; i++) {
-    arr.push(arr[i]);
-}   
-
-console.log(ans);
+// ___________________________withou use 
 
 
+// const arr=[10,20,30,40,50];
+// const arr1=[11,22,33,44,55];
+
+// let ans=[];
+
+// for(let i=0; i<arr.length; i++) {
+//     ans.push(arr[i]);
+// }   
+
+// for(let i=0; i<arr.length; i++) {
+//     ans.push(arr1[i]);
+// }  
+
+// console.log(ans);
+
+
+// ___________________________________________________________________ 11
+
+// 11.    Splitting an array into two arrays based on a condition. 
+
+//  const arr=[10,50,99,10,87,58,42,38,98 ,10];
+
+
+//  ans=[];
+//  ans1=[];
+
+//  for(let i=0; i<arr.length; i++){
+//     if(arr[i] > 50) {
+//         ans.push(arr[i]);
+//     } else {
+//         ans1.push(arr[i]);
+//     }
+//  }
+
+//  console.log(ans,ans1);
+
+// ___________________________________________________________________ 12
+
+// 12.    Rotating an array by a given number of positions. 
+
+// n=parseInt(prompt(("enter num")));
+// const arr=[10,20,30,40,50,60,70,80,90,100]; 
+
+// const ans=arr.splice(arr.length-n,n)
+
+// console.log(ans);
+
+// for (let i=ans.length-1; i>=0; i--) {
+//     arr.unshift(ans[i])
+// }
+
+// console.log(arr);
+
+
+// ________________________________________ 
+
+
+// n=parseInt(prompt(("enter num")));
+// const arr=[10,20,30,40,50,60,70,80,90,100]; 
+
+// const ans=arr.splice(0,n);
+
+// console.log("arr1",ans);
+
+// for(let i=0; i<ans.length; i++) {
+//    arr.push(ans[i])
+// }
+
+
+// console.log("ans",arr);
+
+// ___________________________________________________________________ 13
+ 
+//  13.    Finding the second largest element in an array. 
+
+// const arr=[10.5,69,87,2,21,47,96,39];
+// const ans=arr.slice().sort((a,b) => b-a) // [96, 87, 69, 47, 39, 21, 10.5, 2]
+// console.log(ans);
+
+// console.log(ans[1]);
+
+//  13.    Finding the second largest element in an array. 
+
+
+// const arr=[10.5,69,87,2,21,9999,47,96,39,];
+
+// let large=0;
+
+// for(let i=0; i<=arr.length; i++) {
+//    if(arr[i]> large) {
+//       large=arr[i]
+//    }
+// }
+
+// console.log(large);
+
+// let secondlarge=0;
+
+// for(let i=0; i<arr.length; i++) {
+//    if(arr[i] > secondlarge && arr[i] < large) {
+//       secondlarge=arr[i]
+//    }
+// }
+
+// console.log(secondlarge);
 
 
 
+// ___________________________________________________________________ 14
+// 14.    Finding the k-th smallest element in an array. 
+
+// let arr=[5,10,45,20,30,25,35,40,15,50]; // [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+
+// let ans=arr.sort((a,b) => a-b );
+
+// console.log(ans);
+
+
+// let k=3; //  3rd smallest element
+
+// console.log(arr[k - 1]);
+// ___________________________________________________________________ 15
+
+// 15.    Finding the median of an array. 
+
+// const arr=[10,20,30,40,50];   odd number 
+
+// const ans=arr.slice(2,3);
+
+// const ans=arr.splice(2,1)
+
+// console.log(ans);
+
+// _______________________ 
+// 15.    Finding the median of an array. 
+
+//  const arr=[10,20,30,40];  
+
+// let arr=[2,3,4,6,5,1]; // 1,2,3,4,5,6
+
+// let ans=arr.sort((a,b) => a - b);
+
+// const ans1 = Math.floor(arr.length / 2);
+
+// console.log(ans1);
+
+
+// if (arr.length % 2 === 0) {
+//   let result=(ans[ans1] + ans[ans1-1]) /2;
+
+//   console.log(result);
+  
+// } else {
+//   console.log(ans[ans1]);
+  
+// }
+
+
+
+// ___________________________________________ 16 
+
+// 16.    Checking if two arrays are equal or not. 
+
+
+
+//  let arr=[10,20,30];
+
+// let arr1=[10,20,30];
+
+// let ans = arr.length === arr1.length && arr.every((value,index) => value === arr1[index])
+
+// console.log(ans);
+
+// ___________________________________________ 17
+
+// 17.    Finding the index of the first occurrence of an element in an array. 
+
+// let arr=[50,100,150,200,250,150,300];
+
+// let ans=arr.indexOf(150);
+
+// console.log(ans);
+
+// ___________________________________________ 18
+
+// 18 Finding the index of the last occurrence of an element in an array.  
+// let arr=[50,100,150,200,250,150,300];
+
+// let ans=arr.lastIndexOf(150);
+
+// console.log(ans);
+// ___________________________________________ 19
+
+// 19.    Removing all occurrences of an element from an array.
+
+
+// const arr=[10,20,30,40,10,50,60,10];
+
+// const ans=arr.filter((v,i)=> v !== 10);
+
+// console.log(ans);
+
+// ___________________________________________ 20
+// 20.    Replacing all occurrences of an element in an array with a new element. 
+
+//  const arr=[10,20,30,40,10,50,60,10];
+
+//  const ans=arr.map((v,i) => v === 10 ? 999 : v);
+
+//  console.log(ans);
+
+ // ___________________________________________ 21
+ 
+// 21.    Creating a new array with the elements of the original array in reverse order, without modifying the original array. 
+
+// const arr=[10,20,30,40,50,60,70,80,90,100]; 
+
+// const ans=arr.slice().reverse();
+
+// console.log(arr,ans);
+
+ // ___________________________________________ 22
+
+//  22.    Checking if an array is sorted in ascending order.
+
+//  const arr=[10,50,87,2,3,64,51,21];
+
+//  const ans=arr.slice().sort((a,b) => a-b);
+
+//   const isascending=arr.every((v,i) => v === ans[i])
+ 
+//  if (isascending) {
+   
+//    log("yes ,number is ascending order.");
+    
+//  } else {
+//     console.log("no ,number is not ascending order.");
+    
+//  }
+
+  // ___________________________________________ 23
+
+  //   23.    Checking if an array is sorted in descending order. 
+
+//   const arr= [300, 250, 200, 150, 150, 100, 50];
+//   const ans=arr.slice().sort((a,b) => b-a);
+
+//   const isdescending=arr.every((v,i) => v === ans[i])
+
+//   if(isdescending) {
+//     console.log("yes , ans is descending order");
+    
+//   } else {
+//      console.log("no , ans is not descending order");
+    
+//   }
+
+//   console.log(ans);
+  
+
+
+
+
+ 
+ // ___________________________________________ 24
+//  24.    Finding the first three maximum number's sum an array. 
+
+// const arr=[50,100,150,200,250,150,300]; // [300, 250, 200, 150, 150, 100, 50]
+
+// const ans=arr.sort((a,b)=> b-a).slice(0,3).reduce((acc,v,i) => acc+v , 0) 
+
+// console.log(ans);
