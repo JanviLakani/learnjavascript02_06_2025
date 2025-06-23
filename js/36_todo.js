973a148621fe918cabb5fb30d4c261fa0dc37746
JanviLakani <lakanijanvi1999@gmail.com>
2025-06-23 23:34:56 +0530

    add_home_work_sort_23_06

--- js/36_todo.js
+++ js/36_todo.js
@@ -1,37 +1,30 @@
 const arr = [];
 
-let update=null  // first null reshe 
+let update = null; // first null reshe
+
+// -----------------------------------------------------------------------------------edit btn
 
 const handleedit = (i) => {
-    // console.log('hello');  // hve tya element sudhi phosava mate index pass karaviperameter ma 
+  // console.log('hello');  // hve tya element sudhi phosava mate index pass karaviperameter ma
 
-    // console.log(i,arr[i]); temni index malvani start thase console log ma 
+  // console.log(i,arr[i]); temni index malvani start thase console log ma
 
-    document.getElementById("todo").value=arr[i]
-    
-    update=i;  // jevu update pr ckick karave eni index mle jashe 
-    
-}
+  document.getElementById("todo").value = arr[i];
+
+  update = i; // jevu update pr ckick karave eni index mle jashe
+};
+
+// -----------------------------------------------------------------------------------delete btn
 
 const handledelete = (i) => {
   // console.log("kjkjkkjk");
 
   arr.splice(i, 1);
 
-//   let print = "";
-
-  //   arr.map((v, i) => {
-  //     print += `
-  //         <li>${v}<button onclick="handledelete(${i})">x</button></li>
-  //     `;
-
-  //     document.getElementById("disk").innerHTML = print;
-  //   });  
-
-  display();  // aa display call etla mete km ke ek remove thaya pachi je baki tena pr pn map apply karva ni hovathi 
-
-
+  display(); // aa display call etla mete km ke ek remove thaya pachi je baki tena pr pn map apply karva ni hovathi
 };
+
+// ----------------------------------------------------------------------------------- display
 
 const display = () => {
   let print = "";
@@ -44,6 +37,8 @@ const display = () => {
   });
 };
 
+// ----------------------------------------------------------------------------------- main value
+
 function handleSubmit() {
   event.preventDefault();
 
@@ -51,63 +46,68 @@ function handleSubmit() {
 
   // console.log(todo);
 
-  // const arr=[];
-
-  // onlu one j malshe click ni sathe old valo
-  // arr je push karvayo chhe e remove thai jashe so glaoble arr banavo
-
-//   arr.push(todo);
+  //   arr.push(todo);
 
   console.log(arr);
 
-//   let print = "";
+  if (arr === "") {
+    document.getElementById("todo").innerHTML = "enter your todo";
+  } else {
+    document.getElementById("todo").innerHTML = "";
 
-//   arr.map((v, i) => {
-//     print += `
-//         <li>${v} <button onclick="handledelete(${i})">x</button></li>
-//     `;
+    if (update === null) {
+      arr.push(todo); // update = null hoi tyare data add
+    } else {
+      arr[update] = todo; // edit ni index pr update = i e ena pr thi index mle
+      update = null;
+    }
 
-//     document.getElementById("disk").innerHTML = print;
-//   });  // nise na function ne call karva thi kam thai jashe 
+  }
 
-if(update === null) {
- arr.push(todo);  // update = null hoi tyare data add
-} else {
-    arr[update]=todo;   // edit ni index pr update = i e ena pr thi index mle 
-    update=null;
+  document.getElementById("todo").value = "";
+
+  display();
+  
 }
 
-document.getElementById("todo").value='';
+// ----------------------------------------------------------------------------------- search todo list
 
+const searchhandle = () => {
+  const search = document.getElementById("search").value;
+  // console.log(search,arr);
 
-display ();
-}
+  const sData = arr.filter((v) =>
+    v.toLowerCase().includes(search.toLowerCase())
+  );
 
-const handleKeyUp = () => {
-    const s=document.getElementById("search").value;
-     // console.log(s,arr); 
+  console.log(search, arr, sData);
 
+  // arr nai same tya ni jm sData aave so
 
-    const sData=arr.filter((v)=> v.toLowerCase().includes(s.toLowerCase()))
-
-    console.log(s,arr ,sData);
-
-    // arr nai same tya ni jm sData aave so 
-
-     let print = "";
-    sData.map((v, i) => {
-
+  let print = "";
+  sData.map((v, i) => {
     print += `
         <li>${v}<button onclick="handleedit(${i})">E</button><button onclick="handledelete(${i})">X</button></li>
     `;
 
     document.getElementById("disk").innerHTML = print;
   });
-    
-}
+};
 
+// ----------------------------------------------------------------------------------- search todo list
 
-console.log("hello");
+const sorthandle = () => {
+  let sortvalue = document.getElementById("sort").value;
 
-console.log("janvi");
+  console.log("sort value :-", arr, sortvalue);
 
+  // arr.sort();
+
+  if (sortvalue === "atoz") {
+    sortvalue = arr.sort();
+  } else {
+    sortvalue = arr.sort().reverse();
+  }
+
+  display();
+};
