465c921bb73a844106aef396d604265c92bc8896
JanviLakani <lakanijanvi1999@gmail.com>
2025-06-27 00:35:02 +0530

    add_Oops_method_h_w_26_06

---
+++ js/40_Oops_method.js
@@ -0,0 +1,309 @@
+// // object
+
+// let car = {
+//     brand : "volvo",
+//     color : "red",
+//     start : function(){
+//        console.log("car started");
+
+//     }
+
+// }
+
+//  console.log(car.brand);        // "valvo"
+//  console.log(car.start());     // ans car started
+
+// let person = {
+//     name : "sima",
+//     age : 22,
+//     show : function(){
+//         console.log("my name is " + this.name  + " my age is " + this.age) ;
+
+//     }
+// }
+
+// person.show()   // ans :-  my name is sima my age is 22
+
+// class
+
+// class Person {
+//     constructor( name , age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     showdata(){
+//         console.log(`${this.name} age is ${this.age}`);
+
+//     }
+// }
+
+// let p1=new Person("ravi",22);
+// let p2=new Person("mina",20);
+// let p3=new Person("mahi" ,15);
+
+// p1.showdata();
+// p2.showdata();
+// p3.showdata();
+
+// -------------------
+
+// let people=[
+//     new Person("ravi",22),
+//     new Person("diya",23),
+//     new Person("mina",24),
+//     new Person("kajal",27),
+//     new Person("ravina",20),
+
+// ]
+
+// for(let p of people) {
+//    p.showdata();
+
+// }
+
+// ---------------------------------------------
+
+// class car{
+//     constructor(name ,color){
+//         this.name=name;
+//         this.color=color;
+//     }
+
+//     show(){
+//         console.log(`car name is ${this.name} and car color is ${this.color}`);
+
+//     }
+// }
+
+// let c1=new car("volvo","black");
+// let c2=new car("kia","white")
+
+// c1.show();
+// c2.show();
+
+// -----------------------------------------------------
+
+// 3 Inheritance
+
+// parent class
+
+// class Person {
+//     constructor(name , age) {
+//         this.name = name;
+//         this.age =age;
+
+//     }
+
+//     showdata(){
+//         console.log(`${this.name} is ${this.age} years old `);
+
+//     }
+// }
+
+// class student extends Person{
+//     constructor(name ,age , grade) {
+//     super(name ,age);
+//     this.grade=grade;
+//     }
+
+//     show(){
+//         console.log(`${this.name} is in grade ${this.grade}`);
+
+//     }
+// }
+
+// let p1=new student("ravi" , 16 ,"10th");
+
+// p1.show()
+// p1.showdata()
+
+// _______________________________________________________
+
+// class employee{
+//     constructor(name , salary){
+//         this.name=name;
+//         this.salary=salary;
+//     }
+
+//     showdata(){
+//         console.log(`employee name is ${this.name} and his salary is ${this.salary}`);
+
+//     }
+// }
+
+// //  // let e1 =new employee("rihan",20000);
+
+// // // e1.showdata();
+
+// class manager extends employee{
+//     constructor(name,salary ,department){
+//         super(name ,salary );
+//         this.department=department;
+//     }
+
+//     mangeteam(){
+//         console.log(`${this.name} is manage a ${this.department} department to company`);
+
+//     }
+// }
+
+// let p2=new manager("siya",20000 ,"sales");
+
+// p2.showdata();  // ans employee name is siya and his salary is 20000
+// p2.mangeteam();  // siya is manage a sales department to company
+
+// ------------------------------------------------------------
+
+// Encapsulation
+
+// class Person {
+//     #age;
+
+//     constructor(name , age) {
+//         this.name=name;
+//         this.age=age;
+
+//     }
+
+//     getage(){
+//         return this.#age;
+//     }
+
+//     setage(newAge){
+//         if(newAge > 0 && newAge < 150) {
+//             this.#age= newAge;
+//         } else {
+//             console.log("invelid age");
+
+//         }
+//     }
+
+//     showDetails(){
+//         console.log(`${this.name} is ${this.age} years old`);
+
+//     }
+// }
+
+// let p1 = new Person("Ravi", 25);
+// p1.showDetails();
+
+// p1.setage(99);
+// console.log(p1.getage());
+
+// p1.setage(-5);
+// console.log(p1.getage());
+
+// ----------------------------------------------------------
+
+// class bankAccount {
+//   #balance;
+
+//   constructor(acountholder, initialBalance) {
+//     this.acountholder = acountholder;
+//     this.#balance = initialBalance;
+//   }
+
+//   // Deposit money (safe)
+
+//   deposit(amount) {
+//     if (amount > 0) {
+//       this.#balance += amount;
+//       console.log(`deposit ${amount}`);
+//     } else {
+//       console.log("invalid deposit amount");
+//     }
+//   }
+
+//   // Withdraw money (safe)
+
+//   withdraw(amount) {
+//     if (amount > 0 && amount <= this.#balance) {
+//       this.#balance -= amount;
+//       console.log(`withdraw ${amount}`);
+//     } else {
+//       console.log(" Not enough balance in your account");
+//     }
+//   }
+
+//   //   Check balance
+
+//   getbalance() {
+//     return this.#balance;
+//   }
+// }
+
+// let acc = new bankAccount("ravi", 2000);
+
+// console.log(acc.acountholder); // ravi
+// console.log(acc.getbalance()); // 2000
+
+// acc.deposit(500); // deposit 500
+// console.log(acc.getbalance()); // 2500
+
+// acc.withdraw(1000); // withdraw 1000
+// console.log(acc.getbalance()); // 1500
+
+// acc.withdraw(3000); //  Not enough balance in your account
+// acc.deposit(-100); // invalid deposit amount
+
+
+// ------------------------------------------------------ 
+
+// Polymorphism   // Polymorphism means the same function with different signatures is called many times. 
+// It allows methods to do different things based on the object it is acting upon. - behavior 
+
+// class Animal{
+//    speak(){
+//     console.log("Animal makes a sound");
+    
+//    }   
+// }
+
+// class dog extends Animal{
+//     speak(){
+//         console.log("Dog barks");
+        
+//     }
+// }
+
+// class cat extends Animal{
+//     speak(){
+//         console.log("Cat meows");
+        
+//     }
+// }
+
+// const dogs=new dog();  
+// dogs.speak();  // Dog barks
+
+// const cats=new cat();
+// cats.speak()   // Cat meows
+
+
+// ------------------------------------------------ 
+
+// Abstraction  
+
+// Sirf important cheez dikhana, aur details chhupana. ex. car fan
+
+class fan{
+    #poweron = false;
+
+    turnon(){
+        this.#poweron=true;
+        console.log("fan is on");
+        
+    }
+
+    turnof(){
+        this.#poweron=false;
+        console.log("fan is off");
+        
+    }
+}
+
+let myfan =new fan();
+
+myfan.turnon();  // fan is on
+myfan.turnof();  // fan is off
