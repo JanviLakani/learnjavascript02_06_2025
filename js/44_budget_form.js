af45c2439087552c0060770a9ab893f34a72ff65
JanviLakani <lakanijanvi1999@gmail.com>
2025-07-07 00:50:53 +0530

    add

--- js/44_budget_form.js
+++ js/44_budget_form.js
@@ -1,6 +1,4 @@
-
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 
-
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 class Budget {
   //3
@@ -125,27 +123,25 @@ class Expenses extends Budget {
 
       console.log(localdata);
 
-      if(this.update === null) {
-         localdata.push(obj);
+      if (this.update === null) {
+        localdata.push(obj);
         this.DisplayBudget(obj);
       } else {
-        const i=localdata.findIndex((v,i) => v.id === this.update )
-        localdata[i]=obj
+        const i = localdata.findIndex((v, i) => v.id === this.update);
+        localdata[i] = obj;
 
-        const parent =document.getElementById("expdata");
-         const oldChild = document.getElementById(`row ${this.update}`);  //jiski id jo hamne click kiya vo this.update me save karta hai 
-         const newChild=this.createUpdatedDiv(obj);
+        const parent = document.getElementById("expdata");
+        const oldChild = document.getElementById(`row ${this.update}`); //jiski id jo hamne click kiya vo this.update me save karta hai
+        const newChild = this.createUpdatedDiv(obj);
 
-         if(oldChild) {
-          parent.replaceChild(newChild,oldChild)
-         }
+        if (oldChild) {
+          parent.replaceChild(newChild, oldChild);
+        }
 
-        this.update=null
+        this.update = null;
       }
 
-      // localdata.push(obj);  // cut 
-
-   
+      // localdata.push(obj);  // cut
 
       localStorage.setItem("expence", JSON.stringify(localdata));
 
@@ -175,16 +171,14 @@ class Expenses extends Budget {
     const amtbtn = document.createElement("button");
     amtbtn.textContent = "E";
 
-    amtbtn.addEventListener("click" , () => {
-      this.Expenses_name.value=obj.name;
-      this.Expenses_amt.value=obj.amount;
+    amtbtn.addEventListener("click", () => {
+      this.Expenses_name.value = obj.name;
+      this.Expenses_amt.value = obj.amount;
 
-      // this.update=null; 
+      // this.update=null;
 
-      this.update=obj.id;
-
-
-    })
+      this.update = obj.id;
+    });
 
     namebtn.addEventListener("click", () => {
       divEle.remove();
@@ -198,7 +192,6 @@ class Expenses extends Budget {
       this.handledata();
     });
 
-
     divEle.appendChild(namespan);
     divEle.appendChild(amtspan);
     divEle.appendChild(namebtn);
@@ -208,47 +201,46 @@ class Expenses extends Budget {
     epen.appendChild(divEle);
   }
 
-
-  // ======================================= 
+  // =======================================
 
   createUpdatedDiv(obj) {
-  const divEle = document.createElement("div");
-  divEle.setAttribute("id", `row ${obj.id}`);
+    const divEle = document.createElement("div");
+    divEle.setAttribute("id", `row ${obj.id}`);
 
-  const namespan = document.createElement("span");
-  namespan.textContent = obj.name;
+    const namespan = document.createElement("span");
+    namespan.textContent = obj.name;
 
-  const amtspan = document.createElement("span");
-  amtspan.textContent = obj.amount;
+    const amtspan = document.createElement("span");
+    amtspan.textContent = obj.amount;
 
-  const namebtn = document.createElement("button");
-  namebtn.textContent = "X";
-  const amtbtn = document.createElement("button");
-  amtbtn.textContent = "E";
+    const namebtn = document.createElement("button");
+    namebtn.textContent = "X";
+    const amtbtn = document.createElement("button");
+    amtbtn.textContent = "E";
 
-  amtbtn.addEventListener("click", () => {
-    this.Expenses_name.value = obj.name;
-    this.Expenses_amt.value = obj.amount;
-    this.update = obj.id;
-  });
+    amtbtn.addEventListener("click", () => {
+      this.Expenses_name.value = obj.name;
+      this.Expenses_amt.value = obj.amount;
+      this.update = obj.id;
+    });
 
-  namebtn.addEventListener("click", () => {
-    divEle.remove();
-    const locaExp = JSON.parse(localStorage.getItem("expence"));
-    const exvalue = locaExp.filter((v) => v.id !== obj.id);
-    localStorage.setItem("expence", JSON.stringify(exvalue));
-    this.handledata();
-  });
+    namebtn.addEventListener("click", () => {
+      divEle.remove();
+      const locaExp = JSON.parse(localStorage.getItem("expence"));
+      const exvalue = locaExp.filter((v) => v.id !== obj.id);
+      localStorage.setItem("expence", JSON.stringify(exvalue));
+      this.handledata();
+    });
 
-  divEle.appendChild(namespan);
-  divEle.appendChild(amtspan);
-  divEle.appendChild(namebtn);
-  divEle.appendChild(amtbtn);
+    divEle.appendChild(namespan);
+    divEle.appendChild(amtspan);
+    divEle.appendChild(namebtn);
+    divEle.appendChild(amtbtn);
 
-  return divEle; 
-}
+    return divEle;
+  }
 
-  // ========================================== 
+  // ==========================================
 }
 
 const e1 = new Expenses();
@@ -259,8 +251,6 @@ window.onload = function () {
   const dis = locaExp.map((v, i) => {
     e1.DisplayBudget(v);
   });
-
-
 };
 
 const expenses = document.getElementById("expenses_form");
@@ -270,4 +260,4 @@ expenses.addEventListener("submit", function () {
   console.log("yes2222");
 
   // e1.handleamt();
-});       
+});
